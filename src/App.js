95936a9273eee04d6afc76ce7d7e8a16434ee612
kundan <987kundanrai@gmail.com>
2021-09-21 23:46:54 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 
-import NewsItem from './NewsItem'
+import NewsItem from '../src/components/NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
